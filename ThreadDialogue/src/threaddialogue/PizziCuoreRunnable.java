0eefcf27bcd9f93b80f7e7d426b1e5b639ebd582
Pizzi <Pizzi@DESKTOP-RT0F026>
2020-03-23 12:17:10 +0100

    solved all

--- ThreadDialogue/src/threaddialogue/PizziCuoreRunnable.java
+++ ThreadDialogue/src/threaddialogue/PizziCuoreRunnable.java
@@ -16,75 +16,125 @@ public class PizziCuoreRunnable implements Runnable {
     private Thread T = null;
 
     private final char nome;
+    private int counter;
 
-    PizziCuoreRunnable(char nome) {
+    PizziCuoreRunnable(char nome, int contatore) {
         this.nome = nome;
+        counter = contatore;
 
     }
 
-    public void stampaThread1() throws InterruptedException {
-        System.out.println("Ciao Thread 2");
-        Thread.sleep(2000);
-        System.out.println("Come va la vita?");
-        Thread.sleep(2000);
-        System.out.println("Andiamo a berci una birra?");
-        Thread.sleep(2000);
-        System.out.println("Perchè?");
-        Thread.sleep(2000);
-        System.out.println("Esagerato è solo un'influenza");
-        Thread.sleep(2000);
-        System.out.println("Davvero? Non lo sapevo");
-        Thread.sleep(2000);
-        System.out.println("Cringe");
-        Thread.sleep(2000);
-        System.out.println("");
-        Thread.sleep(2000);
-        System.out.println("");
-        Thread.sleep(2000);
-        System.out.println("");
+    void stampaThread(int contatore) throws InterruptedException {
+        switch (contatore) {
+            case 1:
+                System.out.println("Ciao Thread 2");
+                break;
+            case 2:
+                System.out.println("Ciao Thread 1");
+                break;
+            case 3:
+                System.out.println("Come va la vita?");
+                break;
+            case 4:
+                System.out.println("Potrebbe andare meglio");
+                break;
+            case 5:
+                System.out.println("Andiamo a berci una birra?");
+                break;
+            case 6:
+                System.out.println("No non possiamo");
+                break;
+            case 7:
+                System.out.println("Perchè?");
+                break;
+            case 8:
+                System.out.println("C'è la pandemia");
+                break;
+            case 9:
+                System.out.println("Esagerato è solo un'influenza");
+                break;
+            case 10:
+                System.out.println("No,  ti sbagli, ora è in tutta Italia");
+                break;
+            case 11:
+                System.out.println("Davvero? Non lo sapevo");
+                break;
+            case 12:
+                System.out.println("Cringe");
+                break;
+            case 13:
+                System.out.println("AHAHAHAH");
+                break;
+            case 14:
+                System.out.println("Cosa ridi?");
+                break;
+            case 15:
+                System.out.println("Non sto ridendo");
+                break;
+            case 16:
+                System.out.println("Diocane");
+                break;
+            case 17:
+                System.out.println("BASTAAA");
+                break;
+            case 18:
+                System.out.println("POGLIANI STAI ZITTO");
+                break;
+            case 19:
+                System.out.println("Neffy Su PH Premium");
+                break;
+            case 20:
+                System.out.println("EREzione PElato");
+                break;
+            default:
+                break;
 
+        }
     }
-
-    public void stampaThread2() throws InterruptedException {
-        Thread.sleep(2000);
-        System.out.println("Ciao Thread 1");
-        Thread.sleep(2000);
-        System.out.println("Potrebbe andare meglio");
-        Thread.sleep(2000);
-        System.out.println("No non possiamo");
-        Thread.sleep(2000);
-        System.out.println("C'è la pandemia");
-        Thread.sleep(2000);
-        System.out.println("No,  ti sbagli, ora è in tutta Italia");
-        Thread.sleep(2000);
-        System.out.println("");
-        Thread.sleep(2000);
-        System.out.println("");
-        Thread.sleep(2000);
-        System.out.println("");
-        Thread.sleep(2000);
-        System.out.println("");
-        Thread.sleep(2000);
-        System.out.println("");
-
-    }
-
+    
+    public char getNome(){
+     return nome;   
+    }    
+    
+    public int getCounter(){
+     return counter;   
+    }    
+    
+//    public boolean isPari(){
+//        
+//    }
+//    
+//    public int isDispari(){
+//        
+//    }
+    
     @Override
     public void run() {
-        if (nome == '1') {
-            try {
-                stampaThread1();
-            } catch (InterruptedException ex) {
-                Logger.getLogger(PizziCuoreRunnable.class.getName()).log(Level.SEVERE, null, ex);
+        try {
+            for (int i = 1; i < 21; i++) {
+                Thread.sleep(1000);
+                if (getNome()=='a') {
+                if (getCounter()%2==0) {             
+                    stampaThread(getCounter());
+                } else{
+                    System.out.print("");
+                } 
+            
+            } else if (getNome()=='b'){
+                if (getCounter()%2!=0) {
+                    stampaThread(getCounter());
+                } else{
+                    System.out.print("");
+                }
             }
-        } else if (nome == '2') {
-            try {
-                stampaThread2();
-            } catch (InterruptedException ex) {
-                Logger.getLogger(PizziCuoreRunnable.class.getName()).log(Level.SEVERE, null, ex);
-            }
+            counter++;
         }
+            
+        }catch (InterruptedException ex) {
+                    Logger.getLogger(PizziCuoreRunnable.class.getName()).log(Level.SEVERE, null, ex);
+                }
 
         //throw new UnsupportedOperationException("Not supported yet.");
     }
 }
+
--- ThreadDialogue/src/threaddialogue/ThreadDialogue.java
+++ ThreadDialogue/src/threaddialogue/ThreadDialogue.java
@@ -7,11 +7,14 @@ package threaddialogue;
 public class ThreadDialogue {
 
     public static void main(String[] args) {
-        PizziCuoreRunnable R1 = new PizziCuoreRunnable('1');
+
+        int contatore = 1;
+
+        PizziCuoreRunnable R1 = new PizziCuoreRunnable('a', contatore);
         Thread T1 = new Thread(R1);
         T1.start();
 
-        PizziCuoreRunnable R2 = new PizziCuoreRunnable('2');
+        PizziCuoreRunnable R2 = new PizziCuoreRunnable('b', contatore);
         Thread T2 = new Thread(R2);
         T2.start();
     }
