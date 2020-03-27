78a9ab3a96356e08892375de93e5876e08ff30ae
Pizzi <Pizzi@DESKTOP-RT0F026>
2020-03-27 11:33:46 +0100

    solved all

---
+++ ThreadDialogue3/src/threaddialogue3/ThreadDialogue3.java
@@ -0,0 +1,15 @@
+
+package threaddialogue3;
+
+/**
+ *
+ * @author Pizzi
+ */
+public class ThreadDialogue3 {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
